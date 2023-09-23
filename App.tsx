dfcfe435055e3a65f3036fa08fddec6b91186b18
Sanduranga <edirisinghest527@gmail.com>
2023-09-24 00:46:17 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+// import axios from 'axios';
+
+function App(): JSX.Element {
+  // type picsum = {
+  //   id: string;
+  //   author: string;
+  //   width: number;
+  //   height: number;
+  //   url: string;
+  //   download_url: string;
+  // };
+  console.log('start');
+
+  // useEffect(() => {
+  //   axios
+  //     .get('https://picsum.photos/v2/list?page=1&limit=2')
+  //     .then(function (response) {
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // });
+  return (
+    <View>
+      <Text>Hiiiiii</Text>
+    </View>
+  );
+}
+
+export default App;
